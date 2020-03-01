7810f757ed2eba286ee4317b0f136e22fd8f82c5
Kailey Lee McIntosh <klee.mcintosh@gmail.com>
2020-03-01 16:00:14 -0500

    CRUD working

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -2,7 +2,7 @@ let CustomerModel = require('../models/customer.model')
 let express = require('express')
 let router = express.Router()
 
-// Create a new customer
+// CREATE a new customer
 router.post('/customer', (req, res) => {
   // req.body
   if (!req.body) {
@@ -25,6 +25,56 @@ router.post('/customer', (req, res) => {
     .catch(err => {
       res.status(500).json(err)
     })
+})
+
+// GET
+router.get('/customer', (req, res) => {
+  if (!req.query.email) {
+    return res.status(400).send("Missing URL parameter: email")
+  }
+  CustomerModel.findOne({
+    email: req.query.email
+  })
+  .then(doc => {
+    res.json(doc)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// UPDATE
+router.put('/customer', (req, res) => {
+  if (!req.query.email) {
+    return res.status(400).send("Missing URL parameter: email")
+  }
+  CustomerModel.findOneAndUpdate({
+    email: req.query.email
+  }, req.body, {
+    new: true
+  })
+  .then(doc => {
+    res.json(doc)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+// DELETE
+router.delete('/customer', (req, res) => {
+  if (!req.query.email) {
+    return res.status(400).send("Missing URL parameter: email")
+  }
+  CustomerModel.findOneAndRemove({
+    email: req.query.email
+  })
+  .then(doc => {
+    res.json(doc)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
 })
 
 module.exports = router
